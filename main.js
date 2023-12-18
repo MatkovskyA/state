843360cd68deeeadab94c54dd78adb8fb3215ee1
MatkovskyA <a.matkovsky14@gmail.com>
2023-12-18 23:30:59 +0300

    Добавлены классы тегам заголовков и параграфа. Исправлены замечания по стилям

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-document.querySelector('.header-burger-btn').onclick = () => {
+document.querySelector('.header-burger-btn').addEventListener('click',  () => {
     document.getElementById('header').classList.toggle('open');
-}
+})
